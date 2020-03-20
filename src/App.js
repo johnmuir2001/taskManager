d11da29154a363f44f05d5c7dfa353b4c2edc4ef
ross-burrows <rossburrows96@gmail.com>
2020-03-20 10:59:15 +0000

    appjs bug fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "./Components/nav";
+import Login from "./Components/login";
+import Signup from "./Components/signup";
+import Back from './Components/return';
 
 function App() {
   return (
-    <div className="App">
-      {/* Shall we begin */}
-    </div>
+    <Router>
+      <div className="Back">
+      <Nav />
+      <Route exact path="/" component={Back} />
+      </div>
+      <div> 
+      <Route path="/login" component={Login} />
+      <Route path="/signUp" component={Signup} />
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
