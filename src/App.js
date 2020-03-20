d9f8512e78439ce3277b2c616ed6a9c9a1f2a3e7
liamsouthall <lsouthall116@gmail.com>
2020-03-20 11:07:24 +0000

    Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Timer from "./components/timer";
 
 function App() {
   return (
     <div className="App">
-      {/* Shall we begin */}
+      <Timer />
     </div>
   );
 }
