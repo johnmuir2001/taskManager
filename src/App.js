ebd115d89ede6b8cce6d95a3075597efb986b35d
John Muir <johnmuir1907@gmail.com>
2020-03-20 17:11:20 +0000

    sort out flow and login style

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Nav from "./components/Nav";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Back from './components/Back';
+import TodoPage from './components/TodoPage';
 
 function App() {
   return (
     <Router>
       <div className="Back">
-      <Nav />
       <Route exact path="/" component={Back} />
       </div>
       <div> 
       <Route path="/login" component={Login} />
       <Route path="/signUp" component={Signup} />
+      <Route path="/TodoPage" component={TodoPage} />
       </div>
     </Router>
   );
--- src/components/TodoPage.js
+++ src/components/TodoPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import TaskInput from './TaskInput'
+import Nav from "../components/Nav";
 import '../css/TodoPage.css'
 
 const TodoPage = () => {
     return (
         <div>
+            <Nav />
             <h1>Task To Do</h1>
             <TaskInput/>
         </div>
