83c17fbf83f8bed7c93df0df381abca76086a5d8
John Muir <johnmuir1907@gmail.com>
2020-03-27 13:35:44 +0000

    Updates

---
+++ precache-manifest.adab95741d8a0d8baa54efe10b98df9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbedc2d92768151b4c9afcd8bc879e7f",
+    "url": "/taskManager/index.html"
+  },
+  {
+    "revision": "bb5610aad51909c944ec",
+    "url": "/taskManager/static/css/main.f6bbac90.chunk.css"
+  },
+  {
+    "revision": "5c9c56c48b47ac0bfbba",
+    "url": "/taskManager/static/js/2.b12ec41f.chunk.js"
+  },
+  {
+    "revision": "2ae24f9c7b8e062800e058f2675a7e4d",
+    "url": "/taskManager/static/js/2.b12ec41f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb5610aad51909c944ec",
+    "url": "/taskManager/static/js/main.729c29df.chunk.js"
+  },
+  {
+    "revision": "7aac0a6c418a9c4994a0",
+    "url": "/taskManager/static/js/runtime-main.688250ab.js"
+  },
+  {
+    "revision": "b5d12eb90b807662675eccfe32badb8a",
+    "url": "/taskManager/static/media/GGWgoGetWork.b5d12eb9.png"
+  }
+]);
