319c89f4c3769b384551620fa06c53ef25d3458a
Michael Clare <michaelclare@michaels-mbp.home>
2020-03-27 10:38:15 +0000

    please be fixed now

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,77 +1,68 @@
-import MainNav from '../components/MainNav';
+import MainNav from "../components/MainNav";
 import "../css/timer.css";
-import logo from "../images/GGWgoGetWork.png";
-const React = require("react");
-const ms = require("pretty-ms");
+import ms from "pretty-ms";
+import React,{Component} from "react";
 
-class Timer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: 0,
-      isOn: false,
-      start: 0
-    };
-    this.startTimer = this.startTimer.bind(this);
-    this.stopTimer = this.stopTimer.bind(this);
-    this.resetTimer = this.resetTimer.bind(this);
+
+class Timer extends Component {
+  state = {}
+
+  componentDidMount() {
+    if (this.props.location) {
+      this.setState({ ...this.props.location.state })
+    } else if (this.props.activeTask) {
+      this.setState({ ...this.props.activeTask })
+    }
   }
-  startTimer() {
-    this.setState({
-      isOn: true,
-      time: this.state.time,
-      start: Date.now() - this.state.time
-    });
-    this.timer = setInterval(
-      () =>
-        this.setState({
-          time: Date.now() - this.state.start
-        }),
-      1
-    );
-  }
-  stopTimer() {
-    this.setState({ isOn: false });
-    clearInterval(this.timer);
-  }
-  resetTimer() {
-    this.setState({ time: 0, isOn: false });
-  }
+
   render() {
+    const { currentTask } = this.state
+    const { startTimer, stopTimer, resetTimer, time, isOn} = this.props
+
     return (
-  <div className="container">
-    <div className="topBanner">
-      <img className="timerlogo" src={logo} alt="logo" />
-    </div>
-    <div className="circlebutton">
-      <div className="times">
-        <h2>Timer</h2>
-        <h3>
-          {ms(this.state.time, {
-            colonNotation: true,
-            secondsDecimalDigits: 0
-          })}
-        </h3>
+      <div className="container">
+        <div className="circlebutton">
+          <div className="task">
+            <h1>Task: {currentTask && currentTask.task}</h1>
+          </div>
+          <div className="times">
+            <h3>
+              timer:{" "}
+              {ms(time, {
+                colonNotation: true,
+                secondsDecimalDigits: 0
+              })}
+            </h3>
+          </div>
+          {currentTask && (
+            <div>
+              {time === 0 ? (
+                <button className="button" onClick={startTimer}>
+                  start
+                </button>
+              ) : null}
+              {time === 0 || isOn ? null : (
+                <button className="button" onClick={startTimer}>
+                  resume
+                </button>
+              )}
+              {time === 0 || !isOn ? null : (
+                <button className="button" onClick={stopTimer}>
+                  stop
+                </button>
+              )}
+              {time === 0 || isOn ? null : (
+                <button className="button" onClick={resetTimer}>
+                  reset
+                </button>
+              )}
+            </div>
+          )}
         </div>
-        <div>
-        {this.state.time === 0 ? (
-          <button className="button"  onClick={this.startTimer}>start</button>
-        ) : null}
-        {this.state.time === 0 || this.state.isOn ? null : (
-          <button className="button"  onClick={this.startTimer}>resume</button>
-        )}
-        {this.state.time === 0 || !this.state.isOn ? null : (
-          <button className="button"  onClick={this.stopTimer}>stop</button>
-        )}
-        {this.state.time === 0 || this.state.isOn ? null : (
-          <button className="button" onClick={this.resetTimer}>reset</button>
-        )}
+        <div className="nav">
+          <MainNav />
         </div>
-        </div>
-    <div className="nav">
-      <MainNav/>
-    </div>
-  </div>
+      </div>
     );
   }
 }
